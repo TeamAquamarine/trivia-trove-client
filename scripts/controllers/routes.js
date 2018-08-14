bceec39518e41378b0b432287f3a134f3345b040
Connor Crossley <34751792+Concross@users.noreply.github.com>
2018-08-14 12:04:49 -0700

    Update routes.js

--- scripts/controllers/routes.js
+++ scripts/controllers/routes.js
@@ -1,6 +1,6 @@
 'use strict';
 // Route for testing purposes
-page('/', (ctx, then) => app.Categories.fetchAll(() => app.Categories.initHomeView(ctx)));
+page('/', (ctx, next) => app.Categories.fetchAll(() => app.Categories.initHomeView(ctx)));
 page('/about-us', () => app.showOnly('#about-us-view'));
 page('/create', () => app.showOnly('#create-view'));
 page('/highscore', () => app.showOnly('#highscore-view'));
