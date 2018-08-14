621f7c7efa9838652953d923739bffd8506371a2
alexlhanson <alexlhanson@gmail.com>
2018-08-14 12:01:07 -0700

    References #17 Change name for sorting function in loadAll for categories

--- scripts/models/categories.js
+++ scripts/models/categories.js
@@ -4,19 +4,20 @@ var app = app || {};
 
 (function(module){
 
+
   function Categories(apiCategoriesData) {
     Object.keys(apiCategoriesData).forEach(key => this[key] = apiCategoriesData[key]);
   };
 
   Categories.all = [];
 
-  Categories.loadAll = options => Categories.all = options
-    .sort(( a, b) => a.category - b.category)
+  Categories.loadAll = catArr => Categories.all = catArr
+    .sort(( a, b) => a.name - b.name)
     .map(category => new Category(category));
 
   Categories.fetchAll = callback => 
     {$.get(`http://localhost:3000/api/v1/categories`)
-      .then(Categories.loadAll)
+      .then(res => Categories.loadAll(res))
       .then(callback)
       .catch(errorCallback);}
 
