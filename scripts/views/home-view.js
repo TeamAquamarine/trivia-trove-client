e623c55278f038030abf7deb84970828d88ffa60
Connor <cacrossley22@gmail.com>
2018-08-14 14:48:48 -0700

    form handler adds amount, category id, and difficulty to url /play/(amount)/(cat)/(diff)

--- scripts/views/home-view.js
+++ scripts/views/home-view.js
@@ -15,6 +15,20 @@ var app = app || {};
       $('#category-select').append($option);
     });
 
+
+    // Add handler here for quiz form that will be making the api call
+    // here is the url that the server will eventually need to build and request from API
+    // https://opentdb.com/api.php?amount=10&category=11&difficulty=easy&type=multiple
+
+    $('#play-api-form').on('submit', () => {
+      event.preventDefault();
+      let category = $('#category-select option:selected').val();
+      let difficulty = $('#difficulty-select option:selected').val();
+      let amount = $('#question-number-select option:selected').val();
+      location.replace(`/play/${amount}/${category}/${difficulty}`);
+    })
+
+
     //Add function here to populate the custom names into pulldown
 
   }
