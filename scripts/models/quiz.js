84c1fedb774464227b995815dc3f5320844ceadc
Connor <cacrossley22@gmail.com>
2018-08-14 20:53:48 -0700

    References #25. API fetch call moved from quiz-view to quiz model for MVC practices

--- scripts/models/quiz.js
+++ scripts/models/quiz.js
@@ -9,6 +9,12 @@ var app = app || {};
     Object.keys(questionData).forEach(key => this[key] = questionData[key]);
   }
 
+  Quiz.fetchQuestions = (ctx) => {
+    $.get(`https://opentdb.com/api.php?amount=${ctx.params.amount}&category=${ctx.params.cat_id}&difficulty=${ctx.params.diff}&type=multiple`)
+      .then(res => module.quizView.initQuizView(res.results))
+      .catch(err => console.error(err));
+  }
+
   module.Quiz = Quiz;
 
 })(app);
